239dbbfa35d3c01b517d983f1236cae915e4aa0f
jela3105 <jela3105@gmail.com>
2023-11-25 14:45:26 -0600

    fix if conditional

--- src/front/parser.py
+++ src/front/parser.py
@@ -23,9 +23,9 @@ class Parser:
                 self.index += 1
                 return expr
             else:
-                raise ValueError(f"Expected ')' but found {self.tokens[self.index]}")
+                raise ValueError(f"Esperado ')' pero no se encontro{self.tokens[self.index]}")
         else:
-            raise ValueError(f"Unexpected token: {token}")
+            raise ValueError(f"(Parse primary) Token inesperado: {token} {self.index}")
 
     def parse_term(self):
         """HOLA"""
@@ -52,7 +52,7 @@ class Parser:
         return left
 
     def parse_assignment(self):
-        """HOLA"""
+        """Runs in case it finds an ID"""
         left = self.parse_primary()
 
         if self.tokens[self.index][0] == '=':
@@ -60,7 +60,19 @@ class Parser:
             right = self.parse_expr()
             return ('=', left, right)
         else:
-            raise ValueError(f"Expected '=' but found {self.tokens[self.index]}")
+            raise ValueError(f"Esperado '=' pero encontrado {self.tokens[self.index]}")
+
+    def parse_comparison_expr(self):
+        """Validates comparison syntaxis"""
+        left = self.parse_expr()
+
+        if self.tokens[self.index][0] in {'==', '!=', '>', '<', '>=', '<='}:
+            op = self.tokens[self.index][0]
+            self.index += 1
+            right = self.parse_expr()
+            return (op, left, right)
+        else:
+            return left
 
     def parse_statement(self):
         """Validates the token between a IF statement, ID or a semicolon(;)"""
@@ -68,26 +80,27 @@ class Parser:
 
         if token[0] == 'IF':
             self.index += 1
-            condition = self.parse_expr()
+            condition = self.parse_comparison_expr()
             if_token = self.parse_statement()
             else_token = self.parse_statement() if self.tokens[self.index][0] == 'ELSE' else None
             return ('IF', condition, if_token, else_token)
 
         if token[0] == 'ID':
-            self.index += 1
+            #self.index += 1
             return self.parse_assignment()
 
         if token[0] == ';':
             self.index += 1
             return None
 
-        raise ValueError(f"Unexpected token: {token}")
+        raise ValueError(f"(parse statement) Token inesperado: {token}")
 
     def parse(self) -> list:
         """Iterate each token to create the AST"""
         statements = []
         while self.tokens[self.index][0] != 'EOF': #stops when find the end of the file
             statement = self.parse_statement()
+            print(f"s {statement}")
             if statement is not None:
                 statements.append(statement)
         return statements
