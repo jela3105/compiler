fd48e0485f6af6029fb7038891094ab737c90d80
jela3105 <jela3105@gmail.com>
2024-01-04 11:14:44 -0600

    Add ELSEIF and ELSE tokens

--- src/front/lexer.py
+++ src/front/lexer.py
@@ -8,7 +8,7 @@ class Lexer:
         self.tokens = []
         self.keywords = ('+', '-', '*', '/', '==', '!=',
                          '>', '<', '>=', '<=','{','}', 
-                         '=', ';', 'IF', 'ELSE', '(', ')',
+                         '=', ';', 'IF', 'ELSE', 'ELSEIF', '(', ')',
                          'AND', 'OR')
 
     def tokenize(self) -> None:
