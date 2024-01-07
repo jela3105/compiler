2bb73b7d5ca5b277a5d041c30818314f1e29c3be
jela3105 <jela3105@gmail.com>
2024-01-07 17:41:36 -0600

    Create asm file from intermediate code

--- src/back/target_code_generator.py
+++ src/back/target_code_generator.py
@@ -9,24 +9,38 @@ class TargetCodeGenerator:
         self._body_target_code = ""
         self._data_target_code = ""
 
+    def create_8086_file(self):
+        """create the file in the same directory where the project is running"""
+        f = open('objetofile.asm','w', encoding="utf-8")
+        self.string_target_code = str(f"""
+.model small
+.stack
+.data
+{self._data_target_code}
+.code
+begin:
+{self._body_target_code} 
+end begin""")
+
+        print(self.string_target_code)
+
+        f.write(self.string_target_code)
+        f.close()
+
     def append_assigment(self, line) -> str:
         """Assing new value to the give variable"""
-        return (
-        str(f"""
-mov Ax, @data 
+        return str(f"""
+mov Ax, @data
 mov Ds, Ax
 mov {line[1]}, {line[2]}d
-            """))
+""")
 
     def generate_8086_code(self):
         """crate 8086 code for each line in intermediate code"""
         for line in self.intermediate_code:
-            if line[0] not in self.symbol_table:
-
-                self._data_target_code += str(f"""
-{line[1]} db ?""")
-
+            if line[1] not in self.symbol_table:
+                self._data_target_code += str(f"""{line[1]} db ?
+""")
                 self.symbol_table[line[1]] = line[2]
             self._body_target_code = self._body_target_code + self.append_assigment(line)
-        print(self._data_target_code)
-        print(self._body_target_code)
+        self.create_8086_file()
